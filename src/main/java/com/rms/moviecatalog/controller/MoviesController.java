6e3806611cbc9034b1b04b4c2f24a3ddc367c968
Dominik Zakrzewski <dominikzakrzewski99@gmail.com>
2020-11-01 17:16:53 +0100

    added /api prefix to api endpoints

--- src/main/java/com/rms/moviecatalog/controller/MoviesController.java
+++ src/main/java/com/rms/moviecatalog/controller/MoviesController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/movies")
+@RequestMapping("/api/movies")
 public class MoviesController {
     private MovieRepository movieRepository = new MovieRepository();
 
