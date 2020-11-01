5d45c3b671bf75794812f0dbc7d7aee6ef9ec8c2
Jakub Sydor <sydorjakub@gmail.com>
2020-11-01 20:12:33 +0100

    Front build path change

---
+++ src/main/frontend/config-overrides.js
@@ -0,0 +1,8 @@
+const path = require('path')
+
+module.exports = {
+    paths: function (paths, env) {
+        const appBuild = path.join(__dirname, '../resources/static');
+        return {...paths, appBuild};
+    },
+}
