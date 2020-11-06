3c6447197d4e58b03c0dfab4eebfcd221a48e6f7
Dominik Zakrzewski <dominikzakrzewski99@gmail.com>
2020-11-06 17:57:47 +0100

    added responsive context

--- src/main/frontend/src/App.jsx
+++ src/main/frontend/src/App.jsx
@@ -3,15 +3,36 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { AppRouter } from './routes';
 import { Layout } from './components/Layout/Layout';
+import { ResponsiveContext } from './contexts/ResponsiveContext';
 
-function App() {
-    return (
-        <BrowserRouter>
-            <Layout>
-                <AppRouter />
-            </Layout>
-        </BrowserRouter>
-    );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.toggleSidebar = () => {
+            this.setState((state) => ({
+                isSidebarShown: !state.isSidebarShown,
+            }));
+        };
+
+        this.state = {
+            isSidebarShown: false,
+            toggleSidebar: this.toggleSidebar,
+            isMobile: false,
+        };
+    }
+
+    render() {
+        return (
+            <BrowserRouter>
+                <ResponsiveContext.Provider value={this.state}>
+                    <Layout>
+                        <AppRouter />
+                    </Layout>
+                </ResponsiveContext.Provider>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
--- src/main/frontend/src/components/SideBar/SideBar.jsx
+++ src/main/frontend/src/components/SideBar/SideBar.jsx
@@ -3,30 +3,35 @@ import { NavLink } from 'react-router-dom';
 import { routes } from '../../routes';
 
 import { SideBarWrapper, SideBarMenu, SideBarMenuItem } from './SideBar.style';
+import { ResponsiveContext } from '../../contexts/ResponsiveContext';
 
 export const SideBar = () => {
     return (
-        <SideBarWrapper>
-            <SideBarMenu>
-                <SideBarMenuItem>
-                    <NavLink
-                        to={routes.Home.path}
-                        exact={true}
-                        activeClassName={'active'}
-                    >
-                        Home
-                    </NavLink>
-                </SideBarMenuItem>
-                <SideBarMenuItem>
-                    <NavLink
-                        to={routes.Movies.path}
-                        exact={true}
-                        activeClassName={'active'}
-                    >
-                        Movies
-                    </NavLink>
-                </SideBarMenuItem>
-            </SideBarMenu>
-        </SideBarWrapper>
+        <ResponsiveContext.Consumer>
+            {({ isSidebarShown, toggleSidebar }) => (
+                <SideBarWrapper className={isSidebarShown ? 'shown' : ''}>
+                    <SideBarMenu>
+                        <SideBarMenuItem onClick={toggleSidebar}>
+                            <NavLink
+                                to={routes.Home.path}
+                                exact={true}
+                                activeClassName={'active'}
+                            >
+                                Home
+                            </NavLink>
+                        </SideBarMenuItem>
+                        <SideBarMenuItem onClick={toggleSidebar}>
+                            <NavLink
+                                to={routes.Movies.path}
+                                exact={true}
+                                activeClassName={'active'}
+                            >
+                                Movies
+                            </NavLink>
+                        </SideBarMenuItem>
+                    </SideBarMenu>
+                </SideBarWrapper>
+            )}
+        </ResponsiveContext.Consumer>
     );
 };
