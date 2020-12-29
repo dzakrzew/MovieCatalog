766db2a3787b4ebeea3757aae4348421a710f679
Jakub Sydor <jakub.sydor@gorrion.io>
2020-12-29 17:47:54 +0100

    update

--- src/main/frontend/src/pages/Movies.jsx
+++ src/main/frontend/src/pages/Movies.jsx
@@ -5,10 +5,30 @@ import { API_ENDPOINTS, APIGet } from '../helpers/API';
 import { PageWrapper } from '../components/PageWrapper';
 
 export const MoviesPage = () => {
-    const [movies, setMovies] = useState([]);
+    const [movies, setMovies] = useState({
+        fanFavorites: [],
+        editorsPicks: [],
+        topPicks: [],
+        recentlyViewed: [],
+    });
+
+    const updateMovieList = (sectionName) => (data) => {
+        setMovies((prevState) => ({ ...prevState, [sectionName]: data }));
+    };
 
     useEffect(() => {
-        APIGet(API_ENDPOINTS.movieList + '?details=true').then(setMovies);
+        APIGet(API_ENDPOINTS.movieList + '?details=true&order=fan-favorites').then(
+            updateMovieList('fanFavorites')
+        );
+        APIGet(API_ENDPOINTS.movieList + '?details=true&order=editors-picks').then(
+            updateMovieList('editorsPicks')
+        );
+        APIGet(API_ENDPOINTS.movieList + '?details=true&order=top-picks').then(
+            updateMovieList('topPicks')
+        );
+        APIGet(API_ENDPOINTS.movieList + '?details=true&order=recently-viewed').then(
+            updateMovieList('recentlyViewed')
+        );
     }, []);
 
     // eslint-disable-next-line
@@ -16,15 +36,23 @@ export const MoviesPage = () => {
 
     return (
         <PageWrapper pageTitle={'Browse'}>
-            <MovieList movies={movies} title={'Top picks'} link={'/top-picks'} />
             <MovieList
-                movies={movies}
+                movies={movies.topPicks}
+                title={'Top picks'}
+                link={'/top-picks'}
+            />
+            <MovieList
+                movies={movies.fanFavorites}
                 title={'Fan favorites'}
                 link={'/fan-favorites'}
             />
-            <MovieList movies={movies} title={editorTitle} link={'/editors-picks'} />
             <MovieList
-                movies={movies}
+                movies={movies.editorsPicks}
+                title={editorTitle}
+                link={'/editors-picks'}
+            />
+            <MovieList
+                movies={movies.recentlyViewed}
                 title={'Recently viewed'}
                 link={'/recently-viewed'}
             />
